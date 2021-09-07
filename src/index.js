7d63446a3ab42fccd36377bd9152631dcce19bf2
MikhailTsyganov <tsyganov.mik@gmail.com>
2021-09-07 11:33:47 +0400

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 import './sass/main.scss';
+import ApiService from './js/fetchCountries.js'
+import countryListTemplate from './templates/country-list.hbs'
+import countryTemplate from './templates/country.hbs'
+import '@pnotify/core/dist/BrightTheme.css';
+const debounce = require('lodash.debounce');
+const { error } = require('@pnotify/core');
+
+
+function myError() {
+    error({
+    text: 'Too many matches found. Please enter a more specific query!',
+    width: '560px',
+        delay: 3000,
+        sticker: false,
+        
+    
+})
+} 
+
+const refs = {
+    inputEl: document.querySelector('.input-country'),
+    containerEl: document.querySelector('.js-country-list')
+}
+
+const apiService = new ApiService()
+
+refs.inputEl.addEventListener('input', debounce(onSearchCountry, 500));
+
+
+
+function onSearchCountry(e) {
+    refs.containerEl.innerHTML = ''
+    apiService.query = e.target.value;
+    console.log(e.target.value);
+   
+    
+    apiService.fetchCountry()
+        .then((data) => { if (data.length <= 10) { return data } })
+        .then(countryListMarkUp)
+        .catch(err => myError(err))
+}
+
+
+function countryListMarkUp(data) {
+    if (data.length === 1) {
+        refs.containerEl.insertAdjacentHTML('beforeend', countryTemplate(...data))
+    } else {
+        refs.containerEl.insertAdjacentHTML('beforeend', countryListTemplate(data))
+    }
+    
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,24 @@
+export default class ApiService {
+    constructor() {
+        this.searchQuery = ''
+    }
+    
+    fetchCountry() {
+        const url = `https://restcountries.eu/rest/v2/name/${this.searchQuery}`;
+
+     return fetch(url).then(r => r.json()).then(data => {
+        
+        
+         return data;
+    })
+    }
+
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery
+    }
+}
