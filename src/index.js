873cb6947fb19061a7db568c38181e0df00e579e
MikhailTsyganov <tsyganov.mik@gmail.com>
2021-09-13 20:25:37 +0300

    fix main js

--- src/index.js
+++ src/index.js
@@ -21,6 +21,22 @@ function myError() {
   })
         
 })
+}
+
+function networkError() {
+    error({
+    text: 'Too many matches found. Please enter a more specific query!',
+    width: '500px',
+    delay: 2000,
+    sticker: false,
+    icon: false,
+    closer: false,
+    stack: new Stack({
+    dir1: 'down', dir2: 'left', // Position from the top left corner.
+    firstpos1: 190, firstpos2: 50 // 90px from the top, 90px from the left.
+  })
+        
+})
 } 
 
 const refs = {
@@ -36,17 +52,33 @@ refs.inputEl.addEventListener('input', debounce(onSearchCountry, 500));
 
 function onSearchCountry(e) {
     refs.containerEl.innerHTML = ''
-    apiService.query = e.target.value;
+    apiService.query = e.target.value.trim();
     console.log(e.target.value);
-   
+   if (!apiService.query) {
+        return
+   }
+    
+    
     
     apiService.fetchCountry()
         .then((data) => {
             if (data.length <= 10 && data.length >= 2) { countryListMarkUp(data) }
-            else if (data.length === 1) {
+            if (data.length === 1) {
             countryMarkUp(data)
-        } else {myError()} })
-        
+            } 
+            if(data.length > 10) { myError() }
+        })
+        .catch(() => {
+      error({text: 'Sorry, something went wrong', width: '500px',
+    delay: 2000,
+    sticker: false,
+    icon: false,
+    closer: false,
+    stack: new Stack({
+    dir1: 'down', dir2: 'left', 
+    firstpos1: 190, firstpos2: 50,
+  }) });
+    })
     
 }
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -10,7 +10,8 @@ export default class ApiService {
         
         
          return data;
-    })
+     })
+         
     }
 
 
